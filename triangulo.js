876e1d84f1a709f0924d3eb4bd28fb0fe47a5569
joel <j.e.escobar23@gmail.com>
2017-03-15 20:16:18 +0000

    areglos triangulo

--- triangulo.js
+++ triangulo.js
@@ -5,3 +5,26 @@ console.log("####");
 console.log("#####");
 console.log("######");
 console.log("#######");
+function principal(pisos){
+  console.log("tri");
+  secundaria();
+  return 
+}
+
+principal();
+principal();
+principal();
+principal();
+function secundaria(pisos2){
+  console.log("tri 2");
+  return
+}
+function tercera(pisos3){
+  console.log("tri 3");
+  return
+}
+tercera();
+function areglaPeo(peo){
+  console.log("#");
+}  
+areglaPeo(8);
