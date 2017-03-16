b84c474a0c3bdac519dd75a053d63720bafad587
joel <j.e.escobar23@gmail.com>
2017-03-16 20:38:33 +0000

    indentar

--- triangulo.js
+++ triangulo.js
@@ -3,7 +3,7 @@ function areglaPeo(peo){
   console.log(peo);
   for(var i = 0;i < peo;i++){
   var jashTag = "#"; 
-    console.log(i += jashTag);
+    console.log(i + = jashTag);
   }
 }  
 areglaPeo(8);
